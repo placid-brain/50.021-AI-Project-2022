c4a7d273e8910fa8091ab9768aef9bf1a18b018e
kuro-tora-ryuu <56596164+kuro-tora-ryuu@users.noreply.github.com>
2022-07-25 10:13:09 +0800

    Update app.py
    Tidy up

--- ai-gui/app.py
+++ ai-gui/app.py
@@ -6,19 +6,6 @@ from sklearn.datasets import load_iris
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 
-
-# iris_data = load_iris()
-# # separate the data into features and target
-# features = pd.DataFrame(
-#     iris_data.data, columns=iris_data.feature_names
-# )
-# target = pd.Series(iris_data.target)
-
-# # split the data into train and test
-# x_train, x_test, y_train, y_test = train_test_split(
-#     features, target, test_size=0.2, stratify=target
-# )
-
 ## TO-DO 1 Load data + preprocessing
 
 ## TO-DO 2 Train model 
