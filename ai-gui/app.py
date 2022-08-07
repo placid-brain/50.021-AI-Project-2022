b5ca07c5746048ba9bcf8f08001053132cf59214
kuro-tora-ryuu <56596164+kuro-tora-ryuu@users.noreply.github.com>
2022-08-07 14:17:57 +0800

    Update and Link GUI w/model

--- ai-gui/app.py
+++ ai-gui/app.py
@@ -26,6 +26,45 @@ import numpy as np
 from sklearn.svm import SVR 
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import GridSearchCV
+import torch.nn as nn
+
+
+
+import torch.nn.functional as F
+
+class DNNRegressor(nn.Module):
+    def __init__(self, n_input_features):
+        super(DNNRegressor, self).__init__()
+        
+        self.linear1 = nn.Linear(n_input_features, 50)
+        
+        self.relu1 = nn.ReLU()
+        
+        self.linear2 = nn.Linear(50,10)
+        
+        self.relu2 = nn.ReLU()
+        
+        self.linear3 = nn.Linear(10,1)
+        
+        
+        
+    def forward(self,x):
+        
+        linear1 = self.linear1(x)
+        
+        relu1= self.relu1(linear1)
+        
+        linear2 = self.linear2(relu1)
+        
+        relu2 = self.relu2(linear2)
+        
+        linear3 = self.linear3(relu2)
+        
+        
+        return linear3
+        
+        
+        
 
 #import lstm 
 
@@ -98,9 +137,9 @@ from sklearn.model_selection import GridSearchCV
 class StreamlitApp:
 
     def __init__(self):
-        with open('svr_model2' , 'rb') as f:
-            a = pickle.load(f)
-        self.model= a
+        with open('final_pickle_ver_6' , 'rb') as f:
+            model = pickle.load(f)
+        self.model= model
         #self.model = RandomForestClassifier() # replace with our model
 
    # def train_data(self):  #train our data
@@ -142,29 +181,39 @@ class StreamlitApp:
 
         st.title("Retweet Prediction")
 
+
+        sentiment_class = st.number_input(label="Input Sentiment Class (i.e. 0 for negative, 1 for neutral, 2 for positive)",min_value=0,max_value=2)
+        retweet_status= st.number_input(label="Input Retweet Status (i.e. 0 for un-retweeted, 1 for retweeted)", min_value=0,max_value=1)
+        day = st.number_input(label="Input Day of Post (i.e. 0-6 for Mon to Sunday)",min_value=0,max_value=6)
+        hour = st.number_input(label="Input Hour of Post (i.e. 0-23)",min_value=0,max_value=23)
+        #author_class = st.number_input(label="author class",min_value=0)
         #text=len(st.text_area("Enter Tweet Content").split())
         #fav=int(st.text_area("Enter No. of Likes"))
 
         #scaler=MinMaxScaler()
 
-        text=st.text_area("Input Tweet Content")
-        date=st.text_area("Input Date")
-        fav=st.number_input("Input Fvaorite Count")
+        #text=st.text_area("Input Tweet Content")
+        #date=st.text_area("Input Date")
+        #fav=st.number_input("Input Fvaorite Count")
+        #dir_sent=st.number_input("Input sentiment value")
 
-        data={'date':[date]}
-        date_conv=pd.to_datetime(data["date"])
-        date_conv = date_conv.map(mdates.date2num)[0]
-        sia = SentimentIntensityAnalyzer()
-        sent=sia.polarity_scores(text)['compound']
+        #data={'date':[date]}
+        #date_conv=pd.to_datetime(data["date"])
+        #date_conv = date_conv.map(mdates.date2num)[0]
+        #sia = SentimentIntensityAnalyzer()
+        #sent=sia.polarity_scores(text)['compound']
 
         
-        st.write(sent)
+        #st.write(sent)
 
         #x_scale=scaler.fit_transform(np.array([date_conv,int(fav),sent])).reshape(1,-1)
         #st.write(x_scale)
         #h0, c0=self.model.init_hidden()
-        st.write("Predicted no. of retweets:",self.model.predict(np.array([date_conv,int(fav),sent]).reshape(1,-1))) #to run input with the model
-
+        #st.write("Predicted no. of retweets:",self.model.predict(np.array([date_conv,int(fav),dir_sent]).reshape(1,-1))) #to run input with the model
+        input=np.array([sentiment_class,retweet_status,day,hour])
+        prediction=self.model(torch.from_numpy(input.astype(np.float32))).item()
+        
+        st.write("Output:",int(prediction))
         return self
 
 
